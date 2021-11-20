215d5ab267a804337a420b319456c474de95eefd
Nabeil <nabeillemar@gmail.com>
2021-11-20 13:02:56 -0800

    commit tree b6a02760b55a0108e5ad5f4773f09468384fab25

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code he
+
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = ("victory")
+
+newHeader.innerHTML = ("Nabeil is the champion")
+
+document.body.appendChild(newHeader)
